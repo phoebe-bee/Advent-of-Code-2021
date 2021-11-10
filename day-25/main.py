fe4b30d7b4f368e6090c4c5d77b556ff5dd7013c
phoebe-bee <phoebe.bee.mcewen@gmail.com>
2021-11-10 15:14:29 +0000

    added initial files with file read boilerplate

---
+++ day-25/main.py
@@ -0,0 +1,29 @@
+"""
+https://adventofcode.com/2021/day/25
+"""
+
+import sys
+
+INPUT_FILE_PATH = "./part_01_example_input.txt"
+
+
+def main():
+	try:
+		with open(INPUT_FILE_PATH) as input_raw:
+			pass
+
+	except IOError as ioe:
+		print("{}\nError opening {}. Terminating process.".format(ioe, INPUT_FILE_PATH), file=sys.stderr)
+		sys.exit(1)
+
+
+def part_1():
+	pass
+
+
+def part_2():
+	pass
+
+
+if __name__ == '__main__':
+	main()
